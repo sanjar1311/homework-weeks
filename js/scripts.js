25226d18c731a82c9d572415df742babd07bd202
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2020-12-10 23:01:19 +0500

    fixed js equals

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,15 @@ elForm.addEventListener('submit', function(evt) {
     elOutput.textContent = 'Monday';
   } else if(userInputValue === 2) {
     elOutput.textContent = 'Tuesday';
-  } else if(userInputValue == 3) {
+  } else if(userInputValue === 3) {
     elOutput.textContent = 'Wednesday';
-  }  else if(userInputValue == 4) {
+  }  else if(userInputValue === 4) {
     elOutput.textContent = 'Thursday';
-  }  else if(userInputValue == 5) {
+  }  else if(userInputValue === 5) {
     elOutput.textContent = 'Friday';
-  }  else if(userInputValue == 6) {
+  }  else if(userInputValue === 6) {
     elOutput.textContent = 'Saturday';
-  }  else if(userInputValue == 7) {
+  }  else if(userInputValue === 7) {
     elOutput.textContent = 'Sunday';
   } else {
     elOutput.textContent = 'Error';
